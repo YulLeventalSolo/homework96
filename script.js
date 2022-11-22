9784e5f817ed5f5246056e3ab81f9a07a41f146c
YulLeventalSolo <114673779+YulLeventalSolo@users.noreply.github.com>
2022-11-22 14:50:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+alert('enter 2 number');
+
+let snumber = [];
+
+
+
+let number1 = prompt('add number 1');
+let number2 = prompt('add number 2');
+   
+if ((number1 % number2) !=0)
+{
+    alert('((number1 % number2) !=0)');
+}
+else
+{
+    alert('((number1 % number2) ==0)');
+}
